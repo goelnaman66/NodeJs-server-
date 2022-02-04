99559bca08e037a361c11d02d2d4e6d1594153e7
goelnaman66 <56230034+goelnaman66@users.noreply.github.com>
2022-02-04 13:07:35 +0530

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const server = http.createServer((req,res)=>{
-    res.end("Hello World!!");
-});
-
-server.listen(3000, ()=>{
-    console.log("Server running at port 3000");
-});
