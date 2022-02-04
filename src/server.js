4b8b4d557c278bedd00e7117335166eaa577de57
goelnaman66 <56230034+goelnaman66@users.noreply.github.com>
2022-02-04 13:37:31 +0530

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import morgan from "morgan";
+import { json, urlencoded } from "body-parser";
+
+const app = express();
+
+app.use(morgan('dev'));
+
+
+app.get('/', (req, res) => {
+    console.log(req.body);
+    res.send({message: "OK"});
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send({message: "OK POST"});
+});
+
+export const start = () => {
+    app.listen(3000, () => {
+        console.log("Server started at 3000");
+    });
+}
+
+
+// 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
